b9a6fbc2355ae4d244e2acb01d16e95fe78d19e6
Andrés De la rosa <andres.dr256@gmail.com>
2020-04-24 16:29:34 -0500

    Se crearon clases y métodos para poder compilarse y posteriormente completar el proyecto.

---
+++ src/com/exceptions/Group.java
@@ -0,0 +1,33 @@
+package com.exceptions;
+
+import java.util.List;
+
+public class Group {
+
+    private int maxOccupancy;
+
+    public Group(int maxOccupancy){
+        this.maxOccupancy = maxOccupancy;
+    }
+
+    public int getMaxOccupancy() {
+        return maxOccupancy;
+    }
+
+    public void add(Student student){
+
+    }
+
+    public List<Student> getStudents(){
+
+        return null;
+    }
+
+    public int availability() {
+        return 0;
+    }
+
+    public double getAverage() {
+        return 0;
+    }
+}
